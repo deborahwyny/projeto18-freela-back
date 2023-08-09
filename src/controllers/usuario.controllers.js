6096683f090a9c6dcced5e74b0eb97fde780a1f7
Deborah Wyny <129215842+deborahwyny@users.noreply.github.com>
2023-08-09 15:07:03 -0300

    correção token, rota post/pets

--- src/controllers/usuario.controllers.js
+++ src/controllers/usuario.controllers.js
@@ -6,17 +6,23 @@ import { v4 as uuid } from 'uuid'
 
 export async function cadastroUser(req, res){
 
-    const  {nome, cpf, telefone, email, senha, confirmar_senha} = req.body
+    const { nome, cpf, telefone, email, senha, confirmar_senha } = req.body;
 
     try {
 
-        const emailValidate = await db.query('SELECT * FROM usuario WHERE email=$1;', [email])
+        const emailValidate = await db.query('SELECT * FROM cadastro WHERE email=$1;', [email])
         if (emailValidate.rows.length !== 0)  return res.status(409).send("email invalido")
 
         const bcryptSenha = bcrypt.hashSync(senha, 3)
 
-        const cadastro = await db.query('INSERT INTO usuario ( email, senha) VALUES ($1, $2);', [email, bcryptSenha])
+        const cadastro = await db.query('INSERT INTO cadastro (nome, cpf, telefone, email, senha, confirmar_senha) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;',
+        [nome, cpf, telefone, email, senha, confirmar_senha])
 
+        const cadastroId = cadastro.rows[0].id
+
+        const usuario = await db.query('INSERT INTO usuario (cadastro_id, email, senha) VALUES ($1, $2, $3);',
+        [cadastroId, email, bcryptSenha])
+        
         res.sendStatus(201)
 
     } catch(err){
