cd07cef915607eca65352ad74d87f4c45010a85d
Deborah Wyny <129215842+deborahwyny@users.noreply.github.com>
2023-08-11 15:44:57 -0300

    adicionado boolean ao postpets

--- src/controllers/pets.controllers.js
+++ src/controllers/pets.controllers.js
@@ -4,7 +4,7 @@ import { db } from "../database/database.connection.js"
 
 export async function postpets(req, res) {
 
-    const {caracteristica, nome_tutor, nome_gatinho, telefone_contato } = req.body
+    const {caracteristica, nome_tutor, nome_gatinho, telefone_contato, disponivel } = req.body
 
     const { authorization } = req.headers;
     const token = authorization?.replace('Bearer ', '');
@@ -20,18 +20,13 @@ export async function postpets(req, res) {
         const user_id = validadeToken.rows[0].usuario_id;
         console.log('user_id',user_id)
 
-
-
         const userInfo = await db.query('SELECT * FROM cadastro WHERE id=$1;', [user_id])
         if (userInfo.rows.length === 0) {return res.status(401).send('Usuário não encontrado')}
         console.log('userInfo',userInfo)
 
-        const postCats = await db.query('INSERT INTO pets (caracteristica, nome_tutor, nome_gatinho, telefone_contato) VALUES ($1, $2, $3, $4);', [caracteristica, nome_tutor, nome_gatinho, telefone_contato ])
-
+        const postCats = await db.query('INSERT INTO pets (caracteristica, nome_tutor, nome_gatinho, telefone_contato, disponivel) VALUES ($1, $2, $3, $4, $5);', [caracteristica, nome_tutor, nome_gatinho, telefone_contato, disponivel ])
 
         res.sendStatus(200)
-
-
 
     } catch(err){
         res.status(500).send(err.message)
@@ -44,9 +39,7 @@ export async function getPets(req, res){
     try {
 
         const getPet = await db.query('SELECT * FROM pets;')
-        console.log("oi", getPet)
         res.send(getPet.rows)
-
 
     } catch(err){
         res.status(500).send(err.message)
--- src/schemma/pets.schemma.js
+++ src/schemma/pets.schemma.js
@@ -4,5 +4,6 @@ export const petsSchema = Joi.object({
     caracteristica: Joi.string().required().trim(),
     nome_tutor: Joi.string().required().trim(), 
     nome_gatinho: Joi.string().required().trim(), 
-    telefone_contato: Joi.string().required().trim()  
+    telefone_contato: Joi.string().required().trim(),
+    disponivel: Joi.boolean().required()
 });
